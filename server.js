fd96861817b4d43df38f0b3721ba6c72842ccb88
Koiwu Beyan <koiwubeyan@Koiwus-Mac-mini.local>
2017-09-02 14:46:31 -0700

    Add a static folder and placed JS in to see if the index.html recognizes the file

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
-
-app.use(express.static('/views'));
-
+app.use(express.static(__dirname + '/static'));
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
@@ -92,7 +90,6 @@ app.get('/pagecount', function (req, res) {
     res.send('{ pageCount: -1 }');
   }
 });
-
 
 // error handling
 app.use(function(err, req, res, next){
