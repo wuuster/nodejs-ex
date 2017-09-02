00706d2f77461fa3160e0291fb3e0ab5a49dee2c
Koiwu Beyan <koiwubeyan@Koiwus-Mac-mini.local>
2017-09-02 14:35:28 -0700

    delete files added to view, set view as a static folder

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+app.use(express.static('/views'));
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
@@ -91,13 +93,12 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
-app.use(express.static('/views'));
 
 // error handling
-/*app.use(function(err, req, res, next){
+app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500).send('Something bad happened!');
-});*/
+});
 
 initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
