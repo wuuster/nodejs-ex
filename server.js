d8326c5be380d045288945200ca22cf939997e34
Koiwu Beyan <koiwubeyan@Koiwus-Mac-mini.local>
2017-09-02 17:46:00 -0700

    Update directory, add reference static

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
-app.get("*", express.static(__dirname + '/static'));
-app.get("*", express.static(__dirname));
-app.get("*", express.static("*"));
+app.use('/static',express.static(path.join(__dirname, 'static')));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
