31efcfb663f08fe8d385dfb535f5a86cfd7b444a
Koiwu Beyan <koiwubeyan@Koiwus-Mac-mini.local>
2017-09-03 05:49:21 -0700

    Update directory, add reference static

--- server.js
+++ server.js
@@ -97,6 +97,10 @@ app.use(function(err, req, res, next){
   res.status(500).send('Something bad happened!');
 });
 
+app.get('/static/styles.bundle.js', function(req, res){
+    res.sendFile('/static/styles.bundle.js');
+});
+
 
 initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
